c72304bd46cdcff2ac81c84cac2e58bfdcc85aa8
ChenJie <530216775@qq.com>
2019-03-03 21:13:22 +0800

    post请求提交

---
+++ Project/src/components/FormSubmit.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <textarea v-model="content"></textarea>
+    <hr />
+    <input type="button" @click="submit" value="留言" />
+  </div>
+</template>
+<script>
+  export default {
+    data () {
+      return {
+        content:''
+      }
+    },
+    methods: {
+      submit: function () {
+        this.$http.post('api/Home/AddComment', { content: this.content }).then((response) => {
+          alert("提交成功!,您刚才的留言是" + response.body.content)
+        }, (response) => {
+          alert("出错了！")
+        })
+      }
+    }
+  }
+</script>
--- Project/src/router/index.js
+++ Project/src/router/index.js
@@ -7,6 +7,7 @@ import BlogsList from '@/components/BlogList'
 import Blog from '@/components/Blog'
 import TwoWayBinding from '@/components/TwoWayBinding'
 import FormInput from '@/components/FormInput'
+import FormSubmit from '@/components/FormSubmit'
 
 Vue.use(Router)
 
@@ -46,6 +47,11 @@ export default new Router({
       path: '/form_input',
       name: 'FormInput',
       component: FormInput
+    },
+    {
+      path: '/form_submit',
+      name: 'FormSubmit',
+      component: FormSubmit
     }
   ]
 })
