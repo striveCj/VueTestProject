8ec7eb008c6bfad40e6c2220eec423242910a41d
ChenJie <530216775@qq.com>
2019-03-30 23:39:57 +0800

    main

--- shop_h5/src/main.vue
+++ shop_h5/src/main.vue
@@ -18,10 +18,23 @@
       }
     },
     mounted() {
-
+      store.dispatch(SET_BASEINFO, { open_id: 'opFELv6YkJkMaH-xFkokTWCs5AlQ' })
+      if (this.suer_info.open_id) {
+        store.dispatch(SET_BASEINFO, this.user_info)
+      } else {
+        store.dispatch(SET_BASEINFO)
+        if (store.state.userinfo.open_id === undefined) {
+          console.info('用户id和open_id不存在，跳转到授权等待页面')
+          this.$router.push({name:'wait_to_shouquan'})
+        } else {
+          console.info('已经有BASEINFO')
+        }
+      }
     },
     watch: {
+      '$route'(val) {
 
+      }
     },
     methods: {
 
