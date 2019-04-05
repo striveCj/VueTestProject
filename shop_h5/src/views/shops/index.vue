da6db4965898039235aeee6672ef25c46fccd265
ChenJie <530216775@qq.com>
2019-04-06 00:58:44 +0800

    首页轮播

---
+++ shop_h5/src/views/shops/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="background">
+    <div class="home">
+      <div class="m_layout">
+        <!--轮播图-->
+        <HomeBannerView></HomeBannerView>
+        <!--导航-->
+        <HomeNavView></HomeNavView>
+
+        <span class="divider" style="height:4px;"></span>
+        <div class="product_top">
+          <div class="product_left">
+            <div>商品列表</div>
+          </div>
+        </div>
+        <span class="divider" style="height:2px;"></span>
+      </div>
+    </div>
+  </div>
+</template>
