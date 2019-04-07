93adf6e56680a610f5abd6d5f031c9ab29ea9902
ChenJie <530216775@qq.com>
2019-04-07 22:01:30 +0800

    导航页

--- shop_h5/src/views/shops/index.vue
+++ shop_h5/src/views/shops/index.vue
@@ -14,7 +14,20 @@
           </div>
         </div>
         <span class="divider" style="height:2px;"></span>
+        <SpecialMarket :id="good.id" :name="good.name" :description="good.description" :image_url="good.image_url" v-for="good in goods" ></SpecialMarket>
+
       </div>
     </div>
+    <NavBottomView :is_shops_index="is_shops_index"></NavBottomView>
   </div>
 </template>
+<script>
+  import { bindEvent, scrollPic } from '../../libs/index.js'
+  import HomeBannerView from '../../components/HomeBanner.vue'
+  import HomeNavView from '../../components/HomeNav.vue'
+  import SpecialMarket from '../../components/SpecialMarket.vue'
+  import NavBottomView from '../../components/NavBottom.vue'
+  export default {
+
+  }
+</script>
