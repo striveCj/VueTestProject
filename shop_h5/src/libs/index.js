7868818dfc729ab772994b30d805b75c36119683
ChenJie <530216775@qq.com>
2019-04-08 23:46:34 +0800

    轮播js

--- shop_h5/src/libs/index.js
+++ shop_h5/src/libs/index.js
@@ -1,3 +1,35 @@
 function $id(id) {
   return document.getElementById(id);
 }
+
+function bindEvent() {
+  var sea = $id("my_search");
+
+  var banner = $id("my_banner");
+
+  var height = banner.offsetHeight;
+
+  window.onscroll = function() {
+    var top = document.body.scrollTop;
+
+    if (top > height) {
+      sea.style.background = "rgba(201,21,35,0.85)";
+    } else {
+      var op = top / height * 0.85;
+      sea.style.background = "rgba(201,21,35," + op + ")";
+    }
+  };
+}
+
+function scrollPic() {
+  var imgBox = document.getElementsByClassName("banner_box")[0];
+  var width = $id("my_banner").offsetWidth;
+  var pointBox = document.getElementsByClassName("point_box")[0];
+  var ols = pointBox.children;
+  var indexx = 1;
+  var time = null;
+  var moveX = 0;
+  var endX = 0;
+  var startX = 0;
+  var square = 0;
+}
