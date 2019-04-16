7bf73f3497eef847b2750c3e7cb9fd2583718f2d
ChenJie <530216775@qq.com>
2019-04-16 21:17:21 +0800

    nav

--- shop_h5/src/components/HomeNav.vue
+++ shop_h5/src/components/HomeNav.vue
@@ -2,7 +2,32 @@
   <div class="home_n">
     <nav class="m_nav">
       <ul>
+        <li class="nav_item">
+          <a href="#" class="nav_item_link">
+            <img src="../assets/images/nav0.png" alt="" />
+            <span>草原特色肉</span>
+          </a>
+        </li>
+        <li class="nav_item">
+          <a href="#" class="nav_item_link">
+            <img src="../assets/images/nav0.png" alt="" />
+            <span>特色果干</span>
+          </a>
+        </li>
 
+        <li class="nav_item">
+          <a href="#" class="nav_item_link">
+            <img src="../assets/images/nav0.png" alt="" />
+            <span>特色瓜子</span>
+          </a>
+        </li>
+
+        <li class="nav_item">
+          <a href="#" class="nav_item_link">
+            <img src="../assets/images/nav0.png" alt="" />
+            <span>特色大米</span>
+          </a>
+        </li>
       </ul>
     </nav>
   </div>
