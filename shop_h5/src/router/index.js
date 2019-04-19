1a6dd51b15b568d26d814038a994d094d33f1821
ChenJie <530216775@qq.com>
2019-04-19 23:50:40 +0800

    修改路由

--- shop_h5/src/router/index.js
+++ shop_h5/src/router/index.js
@@ -2,11 +2,17 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import Index from '@/views/shops/index'
+import GoodsDetails from '@/views/shops/goods_details'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
+    {
+      path:'/shops/goods_details',
+      name:'GoodsDetails',
+      compoent:GoodsDetails
+    },
     {
       path: '/wait_to_shouquan',
       name: 'wait_to_shouquan',
