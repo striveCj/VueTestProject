5bb999939bb0a9e06f0e3c1e985dcc96833920ae
ChenJie <530216775@qq.com>
2019-04-21 23:37:08 +0800

    组件编写完成

--- shop_h5/src/views/shops/goods_details.vue
+++ shop_h5/src/views/shops/goods_details.vue
@@ -35,8 +35,42 @@
               </div>
             </section>
 
+            <span class="divider" style="height: 2px;">
+              
+            </span>
+            <div id="choose_number" style="height: 40px;background-color: #fff;">
+              <label style="font-size: 18px;float: left; margin-left:10.5px; margin-top: 7.5px;">购买数量</label>
+              <div style="padding-top: 5px;">
+                <img src="../../assets/add@2x.png" style="margin-right: 10px;display: inline;float: right;width: 30px;" class="plus" @click="plus" alt="">
+                <input pattern="[0-9]*" v-model="buy_count" type="text" name="counts" style="width: 30px;display: inline;float: right;border: 0.5px solid #e2e2e2;line-height: 28px;text-align: center;">
+                <img src="../../assets/minus@2x.png" style="display: inline;float: right;width: 30px;" class="minus" @click="minus" alt="">
+              </div>
+            </div>
+
+            <section class="product_intro">
+              <div class="pro_det" v-html="good.description" style="padding: 0 6.5px;">
+
+              </div>
+            </section>
+            
           </main>
         </div>
+        <footer class="cart_d_footer">
+          <div class="m">
+            <ul class="m_box">
+              <li class="m_item">
+                <a @click="toCart" class="m_item_link">
+                  <em class="m_item_pic three"></em>
+                  <span class="m_item_name">购物车</span>
+                </a>
+              </li>
+            </ul>
+            <div class="btn_box clearfix">
+              <a @click="addToCart" class="buy_now">加入购物车</a>
+              <a @click="zhifu" class="buybuy">立即购买</a>
+            </div>
+          </div>
+        </footer>
       </div>
     </div>
 </template>
