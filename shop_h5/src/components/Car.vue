aeaee074c3b81b2f69077d1b6fd13b7404b41946
ChenJie <530216775@qq.com>
2019-04-25 00:01:34 +0800

    购物车页面增加

---
+++ shop_h5/src/components/Car.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="background">
+      <div id="my_cart">
+        <CartHeaderView></CartHeaderView>
+        <CartMainView></CartMainView>
+        <NavBottomView :is_cart="is_cart"></NavBottomView>
+      </div>
+    </div>
+</template>
+
+<script>
+  import CartHeaderView from './CartHeader.vue';
+  import CartMainView from './CartMain.vue';
+  import NavBottomView from './NavBottom.vue';
+    export default {
+        data(){
+          return{
+            is_cart:true
+          }
+        },
+      mounted(){
+
+      },
+      components:{
+          CartHeaderView,
+        CartMainView,
+        NavBottomView
+      }
+    }
+</script>
+
+<style scoped>
+
+</style>
