f367e26f19c2e8badb77101dde15853091065665
ChenJie <530216775@qq.com>
2019-04-25 23:26:28 +0800

    购物车页面增加

---
+++ shop_h5/src/components/CartHeader.vue
@@ -0,0 +1,18 @@
+<template>
+    <div id="carttp">
+      <header class="top_bar">
+        <a  class="icon_back" onclick="window.history.go(-1)"></a>
+        <h3 class="cartname">购物车</h3>
+      </header>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "CartHeader.vue"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ shop_h5/src/components/CartMain.vue
@@ -0,0 +1,25 @@
+<template>
+    <main class="cart_box">
+      <p v-show="!products.length"><i>请选择商品加入到购物车</i></p>
+      <div class="cart_content clearfix" v-for="item in products" style="position: relative">
+        <div class="cart_shop clearfix">
+          <div class="cart_check_box">
+            <div class="check_box"></div>
+          </div>
+          <div class="shop_info clearfix">
+            <span class="shop_name" style="font-size: 14px">丝路乐购新疆商城</span>
+          </div>
+        </div>
+      </div>
+    </main>
+</template>
+
+<script>
+    export default {
+        name: "CartMain.vue"
+    }
+</script>
+
+<style scoped>
+
+</style>
