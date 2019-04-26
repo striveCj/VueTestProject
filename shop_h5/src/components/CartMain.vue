44dc980c769d37855f6e68472aae892a8cdd3068
ChenJie <530216775@qq.com>
2019-04-26 23:34:36 +0800

    购物车页面增加

--- shop_h5/src/components/CartMain.vue
+++ shop_h5/src/components/CartMain.vue
@@ -10,6 +10,31 @@
             <span class="shop_name" style="font-size: 14px">丝路乐购新疆商城</span>
           </div>
         </div>
+        <div class="cart_del" @click="find_item_id(item)" class="cart_del clearfix" style="display: inline-block;position: absolute;top:10px;right: 10px;">
+          <div class="del_top"></div>
+          <div class="del_bottom"></div>
+        </div>
+        <div class="cart_item">
+          <div class="cart_item_box">
+            <div class="check_box"></div>
+          </div>
+          <div class="cart_detial_box clearfix">
+            <a href="" class="cart_product_link">
+              <img :src="item.image" alt="">
+            </a>
+            <div class="item_names">
+              <a href="">
+                <span></span>
+              </a>
+            </div>
+            <div class="cart_weight">
+              <span class="my_color" style="color:#979292;"></span>
+            </div>
+            <div class="cart_product_sell">
+              <span class="product_money">￥<strong class="real_money"></strong></span>
+            </div>
+          </div>
+        </div>
       </div>
     </main>
 </template>
