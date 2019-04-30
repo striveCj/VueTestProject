d0086dde3106d298d308a62ca1c27b8e6aff957a
ChenJie <530216775@qq.com>
2019-05-01 00:08:10 +0800

    支付页面

--- shop_h5/src/shops/pay.vue
+++ shop_h5/src/shops/pay.vue
@@ -4,7 +4,14 @@
         <a onclick="window.history.go(-1)" class="icon_back"></a>
         <h3 class="cartname">订单支付</h3>
       </header>
-      <div class="tast_list_bd" style="background-color:#f3f3f3;padding-top: 0;: "></div>
+      <div class="tast_list_bd" style="background-color:#f3f3f3;padding-top: 0;padding-bottom:80px; ">
+        <div class="goods_detail" style="">
+          <main class="detail_box">
+            <span class="divider"></span>
+            <form style="margin-top: 45px;"></form>
+          </main>
+        </div>
+      </div>
     </div>
 </template>
 
